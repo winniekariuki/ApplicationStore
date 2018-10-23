499e1b90d410d1e0483aaf56fd3d8197832741f4
winniekariuki <winniekariuki07@gmail.com>
2018-10-23 08:03:58 +0300

    [Feature #161358303] add new files

---
+++ instance/config.py
@@ -0,0 +1,23 @@
+class Config():
+    # Parent confuguration file
+    DEBUG = False
+    SECRET_KEY = "thisissecret"
+
+class Development(Config):
+    '''Configuration for development environment'''
+    DEBUG = True
+
+class Testing(Config):
+    '''Configuration for testing environment'''
+    WTF_CSRF_ENABLED = False
+    DEBUG = True
+
+class Production(Config):
+    '''Configuration for production environment'''
+    DEBUG = False
+
+app_config = {
+    'development': Development,
+    'testing': Testing,
+    'production': Production
+}
